43f16f19ae2812ffcc95ae3e8fa135cbcf6e8dfb
moosaei <mohammad.moosaei@gmail.com>
2022-02-04 12:22:45 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://3b34c4c7.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
